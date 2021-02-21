3e612b43d154896ae85caf50fe912a3735be9391
kayaondul <kayaondul@gmail.com>
2021-02-22 01:38:00 +0300

    Add SplitIntoSubstring For Java 8 and IsPalindrome Methods

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -3,31 +3,40 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class StringUtil {
-    /*
-     *
-     * Java 9 Solution
+    /**
+     * @param s      String to  split
+     * @param chunks chunk sizes to get
+     * @return List of splitted substrings
      */
-    public static List<String> splitStringIntoSubstring_Java9(String s, int chunksize){
-        List<String> strings=new ArrayList<>();
+    public static List<String> splitStringIntoSubstring_Java9(String s, int chunks) {
+        List<String> strings = new ArrayList<>();
 
-        IntStream.iterate(0, i->i+3)
-                .takeWhile(n-> n<s.length())
-                .forEach(num->{
-                    strings.add(s.substring(num,num+3));
-                });
+        IntStream.iterate(0, n -> n < s.length(), i -> i + chunks)
+                .forEach(num -> strings.add(s.substring(num, num + 3)));
 
-        return  strings;
+        return strings;
     }
 
-    public static List<String> splitStringIntoSubstring_Java8(String s, int chunksize){
-        List<String> strings=new ArrayList<>();
-        IntStream.iterate(0, i->i+3)
-                .takeWhile(n-> n<s.length())
-                .forEach(num->{
-                    strings.add(s.substring(num,num+3));
-                });
+    /**
+     * @param s      String to  split
+     * @param chunks chunk sizes to get
+     * @return List of splitted substrings
+     */
+    public static List<String> splitStringIntoSubstring_Java8(String s, int chunks) {
+        List<String> strings = new ArrayList<>();
+        IntStream.iterate(0, i -> i + chunks)
+                .peek(num -> strings.add(s.substring(num, num + chunks)))
+                .allMatch(num -> num < s.length() - chunks);
 
-        return  strings;
+        return strings;
+    }
+
+    public static boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end && s.charAt(start++) == s.charAt(end--)) ;
+        return start >= end;
     }
 
 }
--- src/test/java/StringUtilTest.java
+++ src/test/java/StringUtilTest.java
@@ -2,7 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilTest {
 
@@ -15,13 +16,22 @@ class StringUtilTest {
 
         assertEquals(expected,actual);
     }
+
     @Test
     void splitStringIntoSubstring_Java8() {
 
-        var actual=StringUtil.splitStringIntoSubstring_Java8("soiuahjdowqjwjeowqjoqwjiewqowjeqiowqeijwqeoww",3);
+        var actual = StringUtil.splitStringIntoSubstring_Java8("soiuahjdowqjwjeowqjoqwjiewqowjeqiowqeijwqeoww", 3);
 
-        var expected= List.of("soi","uah","jdo","wqj","wje","owq","joq","wji","ewq","owj","eqi","owq","eij","wqe","oww");
+        var expected = List.of("soi", "uah", "jdo", "wqj", "wje", "owq", "joq", "wji", "ewq", "owj", "eqi", "owq", "eij", "wqe", "oww");
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isPalindRomeTest() {
+
+        assertTrue(StringUtil.isPalindrome("madam"));
+        assertTrue(StringUtil.isPalindrome("kabak"));
+        assertTrue(StringUtil.isPalindrome("ey edip adanada pide ye"));
     }
 }
